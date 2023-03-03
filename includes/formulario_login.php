62b6fc18e2b68c7b4dd0d1e32e62cb46b0a3a104
jonmelo28 <jonmelo28@gmail.com>
2023-03-03 17:49:46 -0300

    second commit

--- includes/formulario_login.php
+++ includes/formulario_login.php
@@ -8,7 +8,7 @@
 
 <div class="jumbotron text-dark bg-light">
 
- <div class="row">
+ <div class="row p-3">
 
    <div class="col">
 
@@ -19,11 +19,11 @@
             <label">E-mail</label>
             <input type="email" name="email" class="form-control" required>
         </div>
-        <div class="form-group">
+        <div class="form-group pt-2">
             <label>Senha</label>
             <input type="password" name="senha" class="form-control" required>
         </div>
-        <div class="form-group">
+        <div class="form-group pt-3">
            <button type="submit" name="acao" value="logar" class="btn btn-primary" >Entrar</button>
         </div>
      </form>
@@ -35,19 +35,19 @@
    <form method="post" >
         <h2 >Cadastre-se</h2>
         <?=$alertaCadastro?>
-        <div >
+        <div>
             <label >Nome</label>
             <input type="text" name="nome" class="form-control" required>
         </div>
-        <div >
+        <div  class="pt-2">
             <label >E-mail</label>
             <input type="email" name="email" class="form-control" required>
         </div>
-        <div class="form-group">
+        <div class="form-group pt-2">
             <label >Senha</label>
             <input type="password" name="senha" class="form-control" required>
         </div>
-        <div class="form-group">
+        <div class="form-group pt-3">
            <button type="submit" name="acao" value="cadastrar" class="btn btn-primary" >Cadastrar</button>
         </div>
      </form>
--- includes/header.php
+++ includes/header.php
@@ -6,28 +6,27 @@ use App\Session\Login;
 $usuarioLogado = Login::getUsuarioLogado();
 
 //função para verificar o usuario e mostra o nome na tela e botão para logar/deslogar do sistema
-$usuario = $usuarioLogado ? $usuarioLogado['nome'].' <a href="logout.php" class="text-light font-weight-bold ml-2">Sair</a>' : 'Visitante';
+$usuario = $usuarioLogado ? $usuarioLogado['nome'].' <a href="logout.php" class="text-light font-weight-bold pl-2"> Sair </a>' : ' Visitante ';
 ?>
 
 <!doctype html>
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>MAG VAGAS</title>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>MAG VAGAS</title>
   </head>
   <body class="bg-dark text-light">
 
   <div class="container">
     <div class="jumbotron bg-danger"> 
-        <h1>MAG VAGAS</h1>
+        <h1 class="p-2">MAG VAGAS</h1>
 
         
         <hr class="border-light">
 
-        <div class="d-flex justify-content-start">
+        <div class="d-flex justify-content-start p-3">
           <?=$usuario?>
         </div>
 
